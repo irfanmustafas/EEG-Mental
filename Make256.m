a4bda8ef96d121a26f6dfb7baacb9f0e60febe5a
Admin <lyi30757@etri.re.kr>
2017-10-17 17:15:01 +0900

    - Implement Make256 to display file path

--- Make256.m
+++ Make256.m
@@ -5,4 +5,19 @@ REP_DIR  = './Rep/';
 
 load('S256.mat');
 
+pSize = size(Sinfo);
+iID = 1; iSym = 2; iGen = 3; iAge = 4; iHav = 10; iDO = 12;
+dPath = '';
 
+for p = 1:pSize(1)
+    if Sinfo(p, iDO), continue, end
+    
+    cLimit = Sinfo(p, iHav);
+    if cLimit == 0, qLimit = 5;
+    else qLimit = cLimit - 1; end
+    
+    for q = 1:qLimit
+        dPath = sprintf('E%03d-%d',Sinfo(p,iID),q);
+        disp(dPath);
+    end
+end
