ba9b27f1057803026b485ff920969b6d5a50fb74
Admin <lyi30757@etri.re.kr>
2017-10-17 19:12:28 +0900

    - Make256: init the participant info

--- Make256.m
+++ Make256.m
@@ -12,11 +12,14 @@ dPath = '';
 for p = 1:pSize(1)
     if Sinfo(p, iDO), continue, end
     
-    cLimit = Sinfo(p, iHav);
+    cLimit = Sinfo(p,iHav);
     if cLimit == 0, qLimit = 5;
     else qLimit = cLimit - 1; end
     
+    pID = Sinfo(p,iID); pSym = Sinfo(p,iSym); pGen = Sinfo(p,iGen); pAge = Sinfo(p,iAge);
+    
     for q = 1:qLimit
+        pVst = q;
         dPath = sprintf('E%03d-%d',Sinfo(p,iID),q);
         disp(dPath);
     end
