92b53b28a00383aaa225655b7c7c88adcd517f89
Admin <lyi30757@etri.re.kr>
2017-10-18 14:18:34 +0900

    - GetPwr: add an EEGLab load set command

--- GetPwr256.m
+++ GetPwr256.m
@@ -7,7 +7,10 @@ REP_DIR  = './Rep/';
 TF.tWin     = 0.50;
 TF.tShift   = 0.10;
 TF.Fs       = Fs;                       % 256
-TF.frange   = [2 50];
+TF.frange   = [0 55];                   % 0~55 Hz 범위
 nWin        = fix(TF.tWin*TF.Fs);       %nWin은 Fs의 절반(0.05)로 설정, 128
 nShift      = fix(TF.tShift*TF.Fs);     %nShift는 Fs의 1/10(0.1)의 정수로 설정, 25
 nFFT        = 2^nextpow2(nWin);     %nWin과 가장 가까원 2의 거듭제곱 수 구하기
+
+set_name = [dPath '.set'];
+EEG         = pop_loadset('filepath', REP_DIR, 'filename', set_name);
