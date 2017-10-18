39a9beecfae5d3dc428c6e6fa788c5657969d70b
Admin <lyi30757@etri.re.kr>
2017-10-18 18:26:19 +0900

    - Make256: implement the initial environment (error state)

--- Make256.m
+++ Make256.m
@@ -8,9 +8,18 @@ elocs    = readlocs('Standard-10-20-Cap2.locs');
 chName   = {elocs.labels}';
 nCh      = length(chName);
 
+REP_DIR  = './Rep/';
+TF.tWin     = 0.50;
+TF.tShift   = 0.10;
+TF.Fs       = 256;                      % 256 Hz Resolution
+TF.frange   = [0 55];                   % 0~55 Hz 범위
+TF.nWin        = fix(TF.tWin*TF.Fs);    % nWin은 Fs의 절반(0.05)로 설정, 128
+TF.nShift      = fix(TF.tShift*TF.Fs);  % nShift는 Fs의 1/10(0.1)의 정수로 설정, 25
+TF.nFFT        = 2^nextpow2(nWin);      % nWin과 가장 가까운 2의 거듭제곱 수 구하기
+TF.f_idx = [];
+
 dPath = '';
 pwr256 = cell(0,6);
-
 
 for p = 1:pSize(1)
     if Sinfo(p, iDO), continue, end
@@ -31,5 +40,23 @@ for p = 1:pSize(1)
         
         disp(dPath);
         SaveSet256(dPath, nCh);
+        
+        if isempty(TF.f_idx)
+            set_name = [dPath '.set'];
+            EEG = pop_loadset('filepath', REP_DIR, 'filename', set_name);
+            
+            % F값 범위 할당 위해 한번 먼저 실행 -> 중복되니 줄일 방법 찾아보는 것도
+            [S,F,T,P]   = spectrogram(double(EEG.data(1,:)),TF.nWin,TF.nWin-TF.nShift,TF.nFFT,TF.Fs);
+            TF.f_idx       = (F>=TF.frange(1)) & (F<=TF.frange(2));    % Frequencey 쳐내기
+            TF.F           = F(f_idx);
+            TF.freq     = F;     nFr   = length(F);
+            TF.time     = T;     nTm   = length(T);
+            % 0~55 Hz 해당하는 Frequencey만 쳐냄, 범위의 딱 절반은 아니고 +/- 1 정도 됨.
+            % F값을 실제로 열어보면 0, 1.953125, 3.90625, 5.859375, 7.81250 ... 순서
+
+        end
+        
+
+
     end
 end
