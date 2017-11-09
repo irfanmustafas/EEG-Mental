6971a67542dd006cd367b0eef42fe2e66c9095ee
Admin <lyi30757@etri.re.kr>
2017-11-09 15:08:20 +0900

    - Make256: Implement the duration variable settings

--- Make256.m
+++ Make256.m
@@ -1,4 +1,4 @@
-% close all; clear; clc;
+close all; clear; clc;
 
 load('S256.mat');
 Sinfo(75,10) = 4;    % E077-4 번부터 2048이라 직접 입력해 줌
@@ -195,74 +195,109 @@ pwr256 = cell(0,6);
 
 
 %% 각 Raw Power 마다 Band 별 Sperctarl Power 계산
+% load([REP_DIR 'WT.mat'])
+% 
+% % 영역 선택, https://en.wikipedia.org/wiki/Electroencephalography
+% gmFreq  = (WT.freq >= 30) & (WT.freq <= 55);
+% muFreq  = (WT.freq >= 8) & (WT.freq < 12);
+% apFreq  = (WT.freq >= 8) & (WT.freq < 15);
+% btFreq  = (WT.freq >= 15) & (WT.freq < 30);
+% thFreq  = (WT.freq >= 4) & (WT.freq < 8);
+% dtFreq  = (WT.freq >= 0.2) & (WT.freq < 4);
+% 
+% % 각 데이터별 값 임시 저장
+% gmWav =  cell(1, 6);
+% muWav =  cell(1, 6);
+% apWav =  cell(1, 6);
+% btWav =  cell(1, 6);
+% thWav =  cell(1, 6);
+% dtWav =  cell(1, 6);
+% 
+% % 각 Band 별 별도 파일 저장
+% for p = 1:pSize(1)
+%     if Sinfo(p, iDO), continue, end
+%     
+%     cLimit = Sinfo(p,iHav);
+%     if cLimit == 0, qLimit = 5;
+%     else qLimit = cLimit - 1; end
+%     
+%     % pID 피험자 ID, pSym 질환 증상, pGen 성별, pAge 나이
+%     pID = Sinfo(p,iID); pSym = Sinfo(p,iSym); pGen = Sinfo(p,iGen); pAge = Sinfo(p,iAge);
+%     
+%     % 실험 프로토콜이 바뀌기 전까지 qLimit 표시까지만, qLimit 부터는 2048Hz
+%     for q = 1:qLimit
+%         % 방문 횟수 pVst
+%         pVst = q;
+%         dPath = sprintf('E%03d-%d',Sinfo(p,iID),q);
+%         
+%         % 예외 처리 (E080-2의 'EEG-.txt'는 EEG-2.txt'로 직접 변경)
+%         % E003-1은 혼자 데이터 길이가 감, E004-1은 2번 채널이 없음
+%         % iAge+q 위치는 데이터가 있는지 없는지 봐서 없는 경우 지나감
+%         if strcmp(dPath, 'E003-1'), continue, end
+%         if strcmp(dPath, 'E004-1'), continue, end
+%         if ~Sinfo(p,iAge+q), continue, end        
+%     
+%         disp(dPath);
+%         load([REP_DIR dPath '_Wav' '.mat'])
+%         
+%         gmPwr   = squeeze(nanmean(Wav(:,gmFreq,:),2));
+%         muPwr   = squeeze(nanmean(Wav(:,muFreq,:),2));
+%         apPwr   = squeeze(nanmean(Wav(:,apFreq,:),2));
+%         btPwr   = squeeze(nanmean(Wav(:,btFreq,:),2));
+%         thPwr   = squeeze(nanmean(Wav(:,thFreq,:),2));
+%         dtPwr   = squeeze(nanmean(Wav(:,dtFreq,:),2));
+%         
+%         gmWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, gmPwr};
+%         muWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, muPwr};
+%         apWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, apPwr};
+%         btWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, btPwr};
+%         thWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, thPwr};
+%         dtWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, dtPwr};
+%         
+%         save([REP_DIR dPath '_gmWav' '.mat'], 'gmWav')
+%         save([REP_DIR dPath '_muWav' '.mat'], 'muWav')
+%         save([REP_DIR dPath '_apWav' '.mat'], 'apWav')
+%         save([REP_DIR dPath '_btWav' '.mat'], 'btWav')
+%         save([REP_DIR dPath '_thWav' '.mat'], 'thWav')
+%         save([REP_DIR dPath '_dtWav' '.mat'], 'dtWav')
+%         
+%     end
+% end
+
+
+%% 자극 구간 설정 후 각 Band별 값
 load([REP_DIR 'WT.mat'])
 
-% 영역 선택, https://en.wikipedia.org/wiki/Electroencephalography
-gmFreq  = (WT.freq >= 30) & (WT.freq <= 55);
-muFreq  = (WT.freq >= 8) & (WT.freq < 12);
-apFreq  = (WT.freq >= 8) & (WT.freq < 15);
-btFreq  = (WT.freq >= 15) & (WT.freq < 30);
-thFreq  = (WT.freq >= 4) & (WT.freq < 8);
-dtFreq  = (WT.freq >= 0.2) & (WT.freq < 4);
+tStart1 = [10 320 630 940 1250 1570];
+t3End1  = tStart1 + 3;
+t5End1  = tStart1 + 5;
+tStart2 = tStart1 + 60;
+t3End2  = tStart2 + 3;
+t5End2  = tStart2 + 5;
+tStart3 = tStart1 + 120;
+t3End3  = tStart3 + 3;
+t5End3  = tStart3 + 5;
 
-% 각 데이터별 값 임시 저장
-gmWav =  cell(1, 6);
-muWav =  cell(1, 6);
-apWav =  cell(1, 6);
-btWav =  cell(1, 6);
-thWav =  cell(1, 6);
-dtWav =  cell(1, 6);
+sm = size(tStart1);
+iStart1 = zeros(sm);
+i3End1  = zeros(sm);
+i5End1  = zeros(sm);
+iStart2 = zeros(sm);
+i3End2  = zeros(sm);
+i5End2  = zeros(sm);
+iStart3 = zeros(sm);
+i3End3  = zeros(sm);
+i5End3  = zeros(sm);
 
-% 각 Band 별 별도 파일 저장
-for p = 1:pSize(1)
-    if Sinfo(p, iDO), continue, end
-    
-    cLimit = Sinfo(p,iHav);
-    if cLimit == 0, qLimit = 5;
-    else qLimit = cLimit - 1; end
-    
-    % pID 피험자 ID, pSym 질환 증상, pGen 성별, pAge 나이
-    pID = Sinfo(p,iID); pSym = Sinfo(p,iSym); pGen = Sinfo(p,iGen); pAge = Sinfo(p,iAge);
-    
-    % 실험 프로토콜이 바뀌기 전까지 qLimit 표시까지만, qLimit 부터는 2048Hz
-    for q = 1:qLimit
-        % 방문 횟수 pVst
-        pVst = q;
-        dPath = sprintf('E%03d-%d',Sinfo(p,iID),q);
-        
-        % 예외 처리 (E080-2의 'EEG-.txt'는 EEG-2.txt'로 직접 변경)
-        % E003-1은 혼자 데이터 길이가 감, E004-1은 2번 채널이 없음
-        % iAge+q 위치는 데이터가 있는지 없는지 봐서 없는 경우 지나감
-        if strcmp(dPath, 'E003-1'), continue, end
-        if strcmp(dPath, 'E004-1'), continue, end
-        if ~Sinfo(p,iAge+q), continue, end        
-    
-        disp(dPath);
-        load([REP_DIR dPath '_Wav' '.mat'])
-        
-        gmPwr   = squeeze(nanmean(Wav(:,gmFreq,:),2));
-        muPwr   = squeeze(nanmean(Wav(:,muFreq,:),2));
-        apPwr   = squeeze(nanmean(Wav(:,apFreq,:),2));
-        btPwr   = squeeze(nanmean(Wav(:,btFreq,:),2));
-        thPwr   = squeeze(nanmean(Wav(:,thFreq,:),2));
-        dtPwr   = squeeze(nanmean(Wav(:,dtFreq,:),2));
-        
-        gmWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, gmPwr};
-        muWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, muPwr};
-        apWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, apPwr};
-        btWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, btPwr};
-        thWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, thPwr};
-        dtWav(1,1:6)    = {pID, pSym, pGen, pAge, pVst, dtPwr};
-        
-        save([REP_DIR dPath '_gmWav' '.mat'], 'gmWav')
-        save([REP_DIR dPath '_muWav' '.mat'], 'muWav')
-        save([REP_DIR dPath '_apWav' '.mat'], 'apWav')
-        save([REP_DIR dPath '_btWav' '.mat'], 'btWav')
-        save([REP_DIR dPath '_thWav' '.mat'], 'thWav')
-        save([REP_DIR dPath '_dtWav' '.mat'], 'dtWav')
-        
-    end
+for t = 1:length(tStart1)
+   [~,iStart1(t)]   = min(abs(WT.time-tStart1(t)));
+   [~,i3End1(t)]    = min(abs(WT.time-t3End1(t)));
+   [~,i5End1(t)]    = min(abs(WT.time-t5End1(t)));
+   [~,iStart2(t)]   = min(abs(WT.time-tStart2(t)));
+   [~,i3End2(t)]    = min(abs(WT.time-t3End2(t)));
+   [~,i5End2(t)]    = min(abs(WT.time-t5End2(t)));
+   [~,iStart3(t)]   = min(abs(WT.time-tStart3(t)));
+   [~,i3End3(t)]    = min(abs(WT.time-t3End3(t)));
+   [~,i5End3(t)]    = min(abs(WT.time-t5End3(t)));
 end
-
-
 
